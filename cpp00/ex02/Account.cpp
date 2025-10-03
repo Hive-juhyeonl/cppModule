8b1327a3b2e10b82fa5ea86f9368cc5b9a76f358
juhyeonl <juhyeonl@student.hive.fi>
2025-10-03 13:31:55 +0300

    add

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: JuHyeon <JuHyeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 20:48:50 by JuHyeon           #+#    #+#             */
-/*   Updated: 2025/10/02 22:13:58 by JuHyeon          ###   ########.fr       */
+/*   Updated: 2025/10/03 13:12:27 by JuHyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,51 @@ int	Account::getNbWithdrawals(void) { return (_totalNbWithdrawals); }
 
 void	Account::makeDeposit(int deposit)
 {
-
+	_amount = _amount + deposit;
+	_totalAmount = _totalAmount + deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_displayTimestamp();
+	std::cout	<< " index:" << _accountIndex
+			<< ";p_amount:" << _amount - deposit
+			<< ";deposit:" << deposit
+			<< ";amount:" << _amount
+			<< ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal(int withdrawal)
 {
+	bool	rst;
 
+	_amount = _amount - withdrawal;
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex
+				<< ";p_amount:" << _amount + withdrawal;
+	if (checkAmount())
+	{
+		rst = true;
+		_totalAmount = _totalAmount - withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		std::cout << ";withdrawal:" << withdrawal
+					<< ";amount:" << _amount
+					<< ";nb_withdrawals:" << _nbWithdrawals;
+	}
+	else
+	{
+		rst = false;
+		_amount = _amount + withdrawal;
+		std::cout << ";withdrawal:refused";
+	}
+	std::cout << std::endl;
+	return (rst);
 }
 
 int Account::checkAmount(void) const
 {
-
+	if (_amount < 0)
+		return (0);
+	return (1);
 }
 
 void    Account::displayStatus(void) const
